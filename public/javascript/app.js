7a2f528847d5a2b0b6c8ce2bb3e6b2becdcdb425
Victor Lawrence <victor@dusk>
2015-11-01 12:01:45 -0500

    Huge overhaul in prep for Angular Material expiraments

---
+++ public/javascript/app.js
@@ -0,0 +1,153 @@
+(function(){
+
+  var app = angular.module("recipeBook", []);
+
+  var bookListing = [
+        {
+          title:"Stuffed Jack-O-Lantern Bell Peppers",
+          date:24929394949929,
+          username:"stepperanddaniel",
+          description:"My own stuffed bell pepper recipe with a festive twist for Halloween.",
+          link:"http://allrecipes.com/recipe/180596/stuffed-jack-o-lantern-bell-peppers/",
+          yield:"6",
+          time:{
+            prep:undefined,
+            cook:"1 hour and 25 minutes"
+          },
+          ingredients:[
+            {
+              quantity:6,
+              unit:"whole",
+              name:"Bell Pepper",
+              note:"any color"
+            },{
+              quantity:1,
+              unit:"pound",
+              name:"ground beef",
+              note:undefined
+            },{
+              quantity:1,
+              unit:undefined,
+              name:"egg",
+              note:undefined
+            },{
+              quantity:"4",
+              unit:"slices",
+              name:"whole wheat bread",
+              note:"cubed"
+            },{
+              quantity:"1",
+              unit:"whole",
+              name:"small onion",
+              note:"chopped"
+            },{
+              quantity:"1",
+              unit:"whole",
+              name:"small tomato",
+              note:"diced"
+            },{
+              quantity:"2",
+              unit:"cloves",
+              name:"garlic",
+              note:"minced"
+            },{
+              quantity:"1/2",
+              unit:"cup",
+              name:"chili sauce",
+              note:undefined
+            },{
+              quantity:"1/4",
+              unit:"cup",
+              name:"prepared yellow mustard",
+              "note":undefined
+            },{
+              quantity:"3",
+              unit:"tablespoons",
+              name:"Worcestershire sauce",
+              note:undefined
+            },{
+              quantity:"1/4",
+              unit:"teaspoons",
+              name:"salt",
+              note:undefined
+            },{
+              quantity:"1/4",
+              unit:"teaspoon",
+              name:"pepper",
+              note:undefined
+            }
+          ],
+          directions:[
+            {value:"Preheat oven to 350 degrees F (175 degrees C). Grease an 8x8 inch baking dish."},
+            {value:"Lightly mix together the ground beef, egg, bread cubes, onion, tomato, garlic, chili sauce, mustard, Worcestershire sauce, salt, and pepper in a bowl."},
+            {value:"Wash the peppers, and cut jack-o'-lantern faces into the peppers with a sharp paring knife, making triangle eyes and noses, and pointy-teeth smiles. Slice off the tops of the peppers, and scoop out the seeds and cores. Stuff the peppers lightly with the beef stuffing, and place them into the prepared baking dish so they lean against each other."},
+            {value:"Bake in the preheated oven until the peppers are tender and the stuffing is cooked through and juicy, about 1 hour."}
+          ],
+          footnotes:"Aluminum foil can be used to keep food moist, cook it evenly, and make clean-up easier."
+        }
+      ];
+
+
+  app.controller("MenuController", function(){
+    this.isShown = false;
+    this.toggleMenu = function(){
+      if (this.isShown === false){ this.isShown = true; }
+      else { this.isShown = false; }
+    }
+  });
+  app.controller("BookController", function(){
+    this.recipes = bookListing;
+  });
+  app.controller("EditModeController", function(){
+    this.isEditMode = true; //default needs to be false.
+    this.toggleEditMode = function(){
+      if (this.isEditMode){
+        this.isEditMode = false;
+      }
+      else{
+        this.isEditMode = true;
+      }
+    }
+
+    this.editedObject = {
+      title:undefined,
+      date:undefined,
+      username:undefined,
+      description:undefined,
+      link:undefined,
+      ingredients:[],
+      directions:[],
+      footnotes:undefined
+    };
+    this.addEmptyIngredientTo = function (targetRecipe) {
+      var emptyIngredient = {
+        quantity:undefined,
+        unit:undefined,
+        name:undefined,
+        note:undefined
+      };
+
+      targetRecipe.ingredients.push(emptyIngredient);
+
+      //console.log("Added Empty Ingredient");
+    }
+
+    this.removeIngredientFrom = function(ingIndex, targetRecipe){
+      targetRecipe.ingredients.splice(ingIndex,1);
+      //console.log("Removed an Ingredient");
+    }
+    this.addEmptyStepTo = function(targetRecipe){
+      var emptyStep ={ value:undefined };
+      targetRecipe.directions.push(emptyStep);
+    }
+    this.removeStepFrom = function(stepIndex,targetRecipe){
+      targetRecipe.directions.splice(stepIndex,1);
+    }
+  });
+
+})()
+$(document).ready(function(){
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+});
