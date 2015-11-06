6f6ca9dc026db7a88c846f91b8d10a459681e835
Victor Lawrence <lawrence_victor@columbusstate.edu>
2015-11-06 02:15:35 -0500

    Another test of the search Action

--- actions/search.js
+++ actions/search.js
@@ -6,8 +6,10 @@ var openshift_DB_pass = process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD;
 var openshift_DB_name = process.env.PGDATABASE;
 var openshift_DB_url  = process.env.OPENSHIFT_POSTGRESQL_DB_URL;
 //var connString = "postgresql://admineie8ym9:pQDGG_EQLTRd@"+openshift_DB_host+":"+openshift_DB_port+"/recipesdb";
-//var connString   = "postgresql://admineie8ym9:pQDGG_EQLTRd@127.7.190.2:5432/recipedb";
-var connString = openshift_DB_url + "/" + openshift_DB_name;
+//var connString = "postgresql://admineie8ym9:pQDGG_EQLTRd@127.7.190.2:5432/recipedb";
+//var connString = "postgresql://admineie8ym9:pQDGG_EQLTRd@127.0.0.1:5432/mytest"
+var connString = "postgresql://" + openshift_DB_user + ":" +
+      openshift_DB_pass + "@" + openshift_DB_host + ":" + openshift_DB_port + "/recipedb";
 
 exports.search = {
   name: 'search',
@@ -24,10 +26,32 @@ exports.search = {
   run: function(api, connection, next){
     var connectionString = connString;
     pg.connect(connectionString, function(err, client, done) {
-    client.query('SELECT 1 + 5', ['brian@example.com'], function(err, result) {
-      connection.response.result.row[0];
+
+      var handleError = function(err) {
+       // no error occurred, continue with the request
+       if(!err) return false;
+
+       // An error occurred, remove the client from the connection pool.
+       // A truthy value passed to done will remove the connection from the pool
+       // instead of simply returning it to be reused.
+       // In this case, if we have successfully received a client (truthy)
+       // then it will be removed from the pool.
+       if(client){
+         done(client);
+       }
+       connection.response.result = {"error message":"An error occured.","error":err};
+       console.log("an error occured \n", err);
+       return true;
+    };
+
+    if(handleError(err)){ return;}
+
+    client.query('SELECT 1 + 5 AS number', ['brian@example.com'], function(err, result) {
       done();  // client idles for 30 seconds before closing
+      connection.response.result.row[0].number;
+      console.log("Success.")
     });
-});
+  });
   }
+
 };
