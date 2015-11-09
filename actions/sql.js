024d66cbd42b6e6d064c06c771b8e6fca1347a5d
Victor Lawrence <victor@dusk>
2015-11-09 17:46:49 -0500

    edited.

--- actions/sql.js
+++ actions/sql.js
@@ -101,8 +101,7 @@ exports.getListOfIngredientsForRecipe = {
     var recipe_id = connection.params.id;
 
     var query =
-    "SELECT * FROM ingredients AS ing, recipeingredientlist AS list WHERE " +
-    "list.recipe_id= " + recipe_id + " AND ing.ingredient_id = list.ingredient_id";
+    "SELECT * FROM ingredients AS ing, recipeingredientlist AS list WHERE ing.ingredient_id = list.ingredient_id AND list.recipe_id= "+recipe_id;
 
     databaseConnect(query, function(err,result){
       if (err){
