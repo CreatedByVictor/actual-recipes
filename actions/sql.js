12625efa145dd2afd72967215f03dbe97b058189
Victor Lawrence <lawrence_victor@columbusstate.edu>
2015-11-10 22:38:59 -0500

    begfax

--- actions/sql.js
+++ actions/sql.js
@@ -379,23 +379,16 @@ exports.addIngredientToRecipe = {
     var ing_quantity  = connection.params.qty;
     var ing_unit      = connection.params.unit || null;
     var param_ing_id  = connection.params.ing_id;
-    //var ing_name      = "'" + connection.params.name + "'";
-    //var ing_name_upper      = ing_name.toUpperCase(); // preformatted for search
     var ing_note      = connection.params.note || null;
     var insertQuery   = "INSERT INTO recipeingredientlist (recipe_id, ingredient_id, quantity, unit, note) VALUES( ${recipe_id}, ${ing_id}, ${ing_quantity}, ${ing_unit}, ${ing_note})";
-    //var getIngredientIdQuery = "SELECT id FROM ingredients WHERE UPPER(name) LIKE ${ingredient_name}";
-    /*
-    var ing_name = connection.params.name;
-    ing_name = "'"+ing_name+"'"; //format query;
-    var getIngredientIdQuery = "SELECT id FROM ingredients WHERE UPPER(name) LIKE " + ing_name.toUpperCase();
-    */
+
     var searchName2 = connection.params.name;
     searchName2 = "'"+searchName2+"'";
-    var getIngredientIdQuery = "SELECT id, name FROM ingredients WHERE UPPER(name) LIKE " + searchName2.toUpperCase();
+    var getIngredientIdQuery = "SELECT id, name FROM ingredients WHERE UPPER(name) LIKE '${ing_name}'";// + searchName2.toUpperCase();
 
     if (param_ing_id === undefined){
       //db.one(getIngredientIdQuery, { "ingredient_name":ing_name_upper }).then(function(data){
-      db.one(getIngredientIdQuery).then(function(data){
+      db.one(getIngredientIdQuery,{"ing_name" = searchName2.toUpperCase()}).then(function(data){
         var ing_id = data[0].id;
         var values = {
           "recipe_id":recipe_id,
