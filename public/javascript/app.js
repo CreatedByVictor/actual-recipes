128b3dcf5c3e4ed328c57c4716050f13a95460f5
Victor Lawrence <lawrence_victor@columbusstate.edu>
2015-11-13 09:55:19 -0500

    spinal tap

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,60 +1,92 @@
-(
-  angular.module('recipeBook')
-  .service("recipeService", ['$http', function($http){
+(function(){
+  var app = angular.module("recipeBook", ['ui.router']);
 
-    this.listIngredients = function(){
+  app.factory("recipeFactory",function($http){
+    var search, ingredients, recipes, recipe, recipeSteps, recipeIngredients;
+    var removeRecipe, removeIngredient, removeRecipeIngredient, removeRecipeStep;
+    var addRecipe, addRecipeStep, addRecipeIng, addIngredient;
+    var updateRecipe, updateRecipeStep, updateRecipeIngredient, updateIngredient;
+    ////////////////////////
+    // Lists and Searches //
+    ////////////////////////
+    ingredients = function(){
       return $http.get('http://blackoak-fogwoods.rhcloud.com/api/listAllIngredients');
     };
-
-    this.listRecipes = function(){
-      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/listAllRecipes');
+    recipes = function(){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/listAllRecipes/');
     };
-
-    this.getRecipe = function(id){
+    recipe = function(id){
       return $http.get('http://blackoak-fogwoods.rhcloud.com/api/getRecipe?recipeid='+id);
     };
-
-  }]);
-
-  angular.module('recipeBook')
-  .config(['$stateProvider', function($stateProvider){
-
-    #stateProvider.state('recipes'{
-      url:"/recipes",
-      resolve:{
-        recipes:['RecipeService', function(RecipeService){
-          return RecipeService.listRecipes();
-        }],
-        recipe:function(){ return {}; }
-      },
-      //templateUrl: '...',
-      controller:'recipeController',
-      controllerAs:'recipes'
-    }).state('search',{
-      url:'/recipes/search/:query',
-      resolve:{
-        recipes:['$stateParams','recipeService',
-          function($stateParams,recipeService){
-            return recipeService.getRecipe($stateParams.query);
-          }],
-        recipe:function(){ return {}; }
-      },
-      //templateUrl: '...',
-      controller:'recipeController',
-      controllerAs:'recipes'
-    })
-
-  }]);
-
-  angular.module('recipeBook')
-  .controller('recipeController', ['$state', 'recipeService', 'recipes', 'recipe', function($state, recipeService, recipes, recipe){
-    this.recipe = recipe.data;
-    this.recipeQuery = $state.params.query;
-    this.recipes = recipes.data;
-    this.getRecipes = function(query){
-      if(!query.length) return $state.go('recipes');
-      $state.go('search',{"id":query});
+    recipeDirections = function(id){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/findRecipeDirections?recipeid='+id);
     };
-  }]);
+    recipeIngredients = function(id){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/findRecipeIngredients?recipeid='+id);
+    };
+    //
+    //
+    //
+    return {
+      ////////////////////////
+      // Lists and Searches //
+      ////////////////////////
+      "ingredients":ingredients,
+      "recipes":recipes,
+      "recipe":recipe,
+      "recipeDirections":recipeDirections,
+      "recipeIngredients":recipeIngredients
+      //
+      //
+      //
+    };
+  });
 
-)()
+  app.directive("recipePanel",function(){
+    return {
+      restrict: 'A',
+      replace:true,
+      templateUrl:"public/directives/recipe-panel.html",
+      controller: function($scope, recipeFactory){
+        $scope.recipes = [];
+        return recipeFactory.recipes().success(function(result){ return $scope.recipes = result; });
+      }
+    }
+  });
+  app.directive("ingredientPanel", function(){
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl:"public/directives/ingredient-panel.html",
+      scope:{ recipeId:'@'},
+      controller:function($scope, recipeFactory){
+        $scope.ingredients = [];
+        var recipe_id = parseInt($scope.recipeId);
+        return recipeFactory.recipeIngredients(recipe_id)
+        .success(function(result){
+          $scope.ingredients = result;
+          return $scope.ingredients;
+        });
+
+      }
+    }
+  });
+  app.directive("directionPanel", function(){
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl:"public/directives/direction-panel.html",
+      scope:{recipeId:'@'},
+      controller:function($scope, recipeFactory){
+        $scope.directions = [];
+        var recipe_id = parseInt($scope.recipeId);
+        return recipeFactory.recipeDirections(recipe_id)
+        .success(function(result){
+          $scope.directions = result;
+          return $scope.directions;
+        });
+      }
+    }
+  })
+
+})()
