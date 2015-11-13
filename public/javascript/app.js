fbd655951eac85247b041ded68e6212c361300c3
Victor Lawrence <lawrence_victor@columbusstate.edu>
2015-11-13 06:20:07 -0500

    working

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,96 +1,60 @@
 (
-  function(){
-    var app = angular.module("recipeBook", ['ngRoute']);
+  angular.module('recipeBook')
+  .service("recipeService", ['$http', function($http){
 
-    app.service("RecipeService",['$http',function($http){
-      this.ListAllIngredients = function(){
-        return $http.get("/api/listAllIngredients");
-      };
-      this.ListAllRecipes = function(){
-        return $http.get("/api/listAllRecipes");
-      };
-      this.Search = function(query){
-        return $http.get("/api/search?q="+query);
-      }
-      this.getRecipe = function(id){
-        return $http.get("/api/getRecipe?id="+id);
-      }
-    }]);
+    this.listIngredients = function(){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/listAllIngredients');
+    };
 
-    app.config('$stateProvider',function($stateProvider){
+    this.listRecipes = function(){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/listAllRecipes');
+    };
 
-      //Recipe List State//////////////////////////
-      $stateProvider
-      .state('recipes', {
-        url:'/recipes',
-        resolve:{
-          recipes:['RecipeService', function(RecipeService){
-            return RecipeService.ListAllRecipes();
+    this.getRecipe = function(id){
+      return $http.get('http://blackoak-fogwoods.rhcloud.com/api/getRecipe?recipeid='+id);
+    };
+
+  }]);
+
+  angular.module('recipeBook')
+  .config(['$stateProvider', function($stateProvider){
+
+    #stateProvider.state('recipes'{
+      url:"/recipes",
+      resolve:{
+        recipes:['RecipeService', function(RecipeService){
+          return RecipeService.listRecipes();
+        }],
+        recipe:function(){ return {}; }
+      },
+      templateUrl: '...',
+      controller:'recipeController',
+      controllerAs:'recipes'
+    }).state('search',{
+      url:'/recipes/search/:query',
+      resolve:{
+        recipes:['$stateParams','recipeService',
+          function($stateParams,recipeService){
+            return recipeService.getRecipe($stateParams.query);
           }],
-          recipe:function(){ return {}; }
-        },
-        templateUrl:'...',
-        controller:'RecipeController';
-        controllerAs: 'recipes'
-      })
+        recipe:function(){ return {}; }
+      },
+      templateUrl: '...',
+      controller:'recipeController',
+      controllerAs:'recipes'
+    })
 
-      //Recipe Search///////////////////////////////
-      .state('search',{
-        url:'/recipe/search/:query',
-        resolve:{
-          recipe: ['$stateParams', 'RecipeService', function($stateParams,RecipeService){
-            return RecipeService.Search($stateParams.query);
-          }],
-          recipe:function() {return {};}
-        },
-        templateUrl: '...',
-        controller:'RecipeController',
-        controllerAs:'recipes'
-      })
+  }]);
 
-      //Edit Recipe//////////////////////////////////
-      .state('recipe',{
-        url:'recipes/:id',
-        resolve:{
-          recipes: function(){return [];},
-          recipe: ['$stateParams', 'RecipeService',
-            function($stateParams, RecipeService){
-              return RecipeService.getRecipe($stateParams.id);
-            }]
-        },
-        templateUrl: '...',
-        controller: 'RecipeController',
-        controllerAs: 'recipes'
-      });
+  angular.module('recipeBook')
+  .controller('recipeController', ['$state', 'recipeService', 'recipes', 'recipe', function($state, recipeService, recipes, recipe){
+    this.recipe = recipe.data;
+    this.recipeQuery = $state.params.query;
+    this.recipes = recipes.data;
+    this.getRecipes = function(query){
+      if(!query.length) return $state.go('recipes');
+      $state.go('search',{"id":query});
+    };
+  }]);
 
-    });
-
-    app.controller('RecipeController', ['$state','RecipeService','recipes', 'recipe', function($state,goatService,recipes,recipe){
-      this.recipe = recipe.data;
-
-      this.recipeQuery = $state.params.query;
-
-      this.recipes = recipes.data;
-
-      this.saveRecipe = function(){
-        RecipeService.saveRecipe(this.recipe)
-          .then(function(){ $state.go('recipes'); });
-      };
-
-      this.searchRecipes = function(query){
-        if (!query.length) return $state.go('recipes');
-        $state.go('search', {query:query});
-      };
-
-    }]);
-
-    app.controller("BookController", function(){
-        this.recipeList = recipes;
-
-        this.UpdateRecipes = function(){
-          getTheRecipes();
-        }
-
-    });
-  }
 )()
